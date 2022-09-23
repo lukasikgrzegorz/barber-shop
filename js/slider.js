94f7804efded122ba30a04e862fcfbf8b011a2e7
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-09-23 23:43:50 +0200

    add js slider new function
    -auto slide
    
    css:
    slide anitamion
    button styles

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,5 @@
 (() => {
-    
+
     const refs = {
         nextBtn: document.querySelector('[slider-next-btn]'),
         prevBtn: document.querySelector('[slider-prev-btn]'),
@@ -9,17 +9,28 @@
     }
 
     let actualImageIndex = 0;
-    let lastImageIndex = refs.images.length-1;
-
-    refs.nextBtn.addEventListener('click', () => {
+    const lastImageIndex = refs.images.length - 1;
+    
+    const nextBtnFunction = () => {
         refs.images[actualImageIndex].classList.add('is-invisible');
+        refs.images[actualImageIndex].classList.add('is-faded');
         actualImageIndex++;
         if (actualImageIndex > lastImageIndex) { actualImageIndex = 0; }
-        refs.images[actualImageIndex].classList.remove('is-invisible');      
-    })
+        refs.images[actualImageIndex].classList.remove('is-invisible');
+    };
+
+    const autoSlide = () => {
+        nextBtnFunction();
+        setTimeout(autoSlide, 5000);
+    };
+
+    autoSlide();
+  
+    refs.nextBtn.addEventListener('click', nextBtnFunction);
 
     refs.prevBtn.addEventListener('click', () => {
         refs.images[actualImageIndex].classList.add('is-invisible');
+        refs.images[actualImageIndex].classList.add('is-faded');
         actualImageIndex--;
         if (actualImageIndex < 0){ actualImageIndex = lastImageIndex; }
         refs.images[actualImageIndex].classList.remove('is-invisible');
